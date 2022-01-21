dc81451f2fed1063ef5c18893aa1afc3ed106956
SaulRosero11 <60784747+SaulRosero11@users.noreply.github.com>
2022-01-21 08:14:53 -0500

    Add files via upload

---
+++ Modelo/Entidades/Amortizacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entidades
+{
+    public class Amortizacion
+    {
+        //Camps de amortizacion
+        public float ValorCredito { get; set; }
+        public float TotalInteres { get; set; }
+        public float TotalCuota { get; set; }
+
+        //Se valida que cumpla con los documentos y las reglas
+        public string EstadoCredito { get; set; }
+
+        //Relacion solicitud
+        public int SolicitudId { get; set; }
+        public Solicitud Solicitud { get; set; }
+    }
+}
---
+++ Modelo/Entidades/DetalleDeudas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entidades
+{
+    public class DetalleDeudas
+    {
+        //Campos de detalle deudas
+        public int DetalleDeudasId { get; set; }
+        public string TragetasCredito { get; set; }
+        public float PagoPendienteTargetas { get; set; }
+        public string CreditosLineaBlanca { get; set; }
+        public float PagoPendienteCreditos { get; set; }
+
+        //Calculo de total de deudas por pagar
+        public float TotalPendienteDeudas { get; set; }
+
+        //Relacion con Solicitante 
+        public int SolicitanteId { get; set; }
+        public Solicitante Solicitante { get; set; }
+
+        //Relacion con Historial Crediticio
+        public ICollection<HistorialCrediticio> HistorialCrediticios { get; set; }
+    }
+}
